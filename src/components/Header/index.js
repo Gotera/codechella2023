8ade522095ad1916b929546a72a1a8de59accf23
OM <orlandomedeiros.junior@gmail.com>
2023-03-05 03:30:48 -0300

    Header component finalized

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import codeChellaLogo from '../../assets/Logo1.png'
+import styles from './header.module.scss'
 
 const Header = () => {
 	return (
 		<>
-			<img src={codeChellaLogo} alt='codechella' />
-			<ul>
-				<li>A experiência</li>
-				<li>Mapa de Setores</li>
-				<li>Informações</li>
-				<li>Ingresso</li>
-			</ul>
+			<div className={styles.container}>
+				<img src={codeChellaLogo} alt='codechella' />
+				<ul>
+					<li>A experiência</li>
+					<li>Mapa de Setores</li>
+					<li>Informações</li>
+					<li>Ingresso</li>
+				</ul>
+			</div>
 		</>
 	)
 }
